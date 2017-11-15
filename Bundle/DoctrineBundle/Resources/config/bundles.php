7ff8668668e6126376491aeb3b16dc50856f236c
Iqbal Maulana <iq.bluejack@gmail.com>
2017-11-15 13:24:28 +0700

    Add DoctrineBundle dependencies on BorobudurDoctrineBundle

---
+++ Bundle/DoctrineBundle/Resources/config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'before' => [
+        Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+        Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true]
+    ]
+];
